43f8a80672e14a4e173ca797dbe25aad3d5fe82c
ephekt <mfrosengarten@gmail.com>
2010-04-06 23:10:10 -0700

    remove references to scratch project, answerwise, and clean up routes.rb, and do a lil stylin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :questions
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   map.resource :account, :controller => "users"
